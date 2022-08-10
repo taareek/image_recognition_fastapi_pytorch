e2d02170bbaa6fb735cf509e65d1529aa30ed375
Tarek Aziz <tarekiub17@gmail.com>
2022-08-10 13:06:18 +0600

    form handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, File, UploadFile, Request
+from fastapi import FastAPI, File, UploadFile, Request, Form
 import utils
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -25,7 +25,20 @@ async def home_predict(request:Request, file:UploadFile= File(...)):
 async def predict(file:UploadFile= File(...)):
     return utils.get_result(img_file=file)
 
+# @app.post("/sub")
+# async def handle_form(top_right_lang: str = Form(...)):
+#     print(top_right_lang)
+
 # some test route
 @app.get("/tarek")
-def tarek():
-    return {"Message": "Hola from Tarek !"}
+async def tarek_home(request:Request):
+    return template.TemplateResponse("test-code.html", {"request": request})
+
+@app.post("/tarek")
+async def tarek(request:Request, top_left_lang: str= Form(...), top_left_long: str= Form(...)):
+    data = {
+        "t_left_lang": top_left_lang,
+        "t_left_long": top_left_long
+    }
+    # return template.TemplateResponse("test-code.html", {"request":request, "top_right_lang": top_right_lang, "top_right_long": top_right_long})
+    return template.TemplateResponse("test-code.html", {"request":request, "data":data})
